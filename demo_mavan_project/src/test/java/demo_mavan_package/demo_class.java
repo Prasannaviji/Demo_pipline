3d5f0aac501c659345e513be3de1149ac0b61076
prasa <prasa@192.168.1.35>
2024-01-04 21:38:10 +0530

    test_maven_demo

---
+++ demo_mavan_project/src/test/java/demo_mavan_package/demo_class.java
@@ -0,0 +1,15 @@
+package demo_mavan_package;
+
+import org.testng.annotations.Test;
+
+public class demo_class {
+	
+	
+	@Test
+	void test1()
+	{
+		
+System.out.println("Test case excuted");
+	}
+
+}
